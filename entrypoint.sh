52ebb17be73f94e41d9c20e14e09213fad5459a9
Bastian <Bastian.Meissner@rz.uni-regensburg.de>
2026-01-29 12:18:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,52 @@
 #!/bin/sh
 
-upgrade_ilias() {
-    if [ "$(ls -A /var/www/html)" ]; then
-        # Move relevant files to a "safe" location.
-        mv /var/www/html/ilias.ini.php /tmp/ilias.ini.php
-        mv /var/www/html/data /tmp/data
-
-        # Nuke /var/www/html.
-        rm -rf /var/www/html/*
-
-        # Bring relevant files back out again.
-        mv /tmp/ilias.ini.php /var/www/html/ilias.ini.php
-        mv /tmp/data /var/www/html/data
-    fi
+delete_entrypoint() {
+    rm -rf /var/www/html/entrypoint.sh
 }
 
-if [ -n "$DEVELOPMENT" ]; then
-    echo "Development mode enabled. Skipping entrypoint.sh."
-else
+upgrade_ilias() {
+    # Move relevant files to a "safe" location.
+    mv /var/www/html/ilias.ini.php /tmp/ilias.ini.php
+    mv /var/www/html/data /tmp/data
+
+    # Nuke /var/www/html.
+    rm -rf /var/www/html/*
+
+    # Copy new ILIAS installation
+    cp -r /app/* /var/www/html
+
+    # Bring relevant files back out again.
+    mv /tmp/ilias.ini.php /var/www/html/ilias.ini.php
+    mv /tmp/data /var/www/html/data
+
+
+    # todo: how do i determine if i am below version 10?
+    # if []; then
+    #     mv /var/www/html/data /var/www/html/public/data
+    #     mkdir -p /var/www/html/public/Customizing/plugins
+    #     mv /var/www/html/Customizing/global/plugins/Services/* /var/www/html/public/Customizing/plugins/
+    #     mv /var/www/html/Customizing/global/plugins/Modules/* /var/www/html/public/Customizing/plugins/
+    # fi
+
+    # /usr/local/bin/php /var/www/html/cli/setup.php update -q -y
+}
+
+install_ilias() {
+    # Copy new ILIAS installation
+    cp -r /app/* /var/www/html
+
+    /usr/local/bin/php /var/www/html/cli/setup.php install /config.json -q -y
+}
+
+
+if [ "$(ls -A /var/www/html)" ] && [ -z "$DEVELOPMENT" ]; then
     upgrade_ilias
+    delete_entrypoint
 fi
 
-# Move ILIAS data to /var/www/html.
-cp -r /app/* /var/www/html
-
-# Delete potentially invalid files.
-rm -f /var/www/html/entrypoint.sh
-
-# Super duper ugly hack that removes google fonts dependency
-# sed -i '/preconnect/,+1d' /var/www/html/Customizing/global/plugins/Services/Repository/RepositoryObject/LongEssayAssessment/vendor/edutiek/long-essay-assessment-service/node_modules/long-essay-assessment-writer/dist/index.html
-# sed -i '/preconnect/,+1d' /var/www/html/Customizing/global/plugins/Services/Repository/RepositoryObject/LongEssayAssessment/vendor/edutiek/long-essay-assessment-service/node_modules/long-essay-assessment-corrector/dist/index.html
-
-if [ -z "$CONFIG_FILE" ]; then
-    CONFIG_FILE="/config/config.json"
+if [ -z "$(ls -A /var/www/html)" ] && [ -z "$DEVELOPMENT" ]; then
+    install_ilias
+    delete_entrypoint
 fi
 
-/usr/local/bin/php /var/www/html/cli/setup.php install -q -y $CONFIG_FILE
-
-# Run apache.
 apache2-foreground
